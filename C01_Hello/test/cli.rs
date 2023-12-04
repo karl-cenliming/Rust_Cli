0127b8640770e63240543912dac8431c08cad6bf
Karl Cen (EXT-NSB) <karl.cen.ext@nokia-sbell.com>
2023-12-04 09:57:44 +0800

    c01 completed page 12 coding

---
+++ C01_Hello/test/cli.rs
@@ -0,0 +1,31 @@
+/*
+--version 1
+#[test]
+fn works(){
+    assert!(true);
+}
+*/
+/*
+-- version 2
+use std::process::Command;
+#[test]
+fn runs(){
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    asset!(res.is_ok());
+}
+ */
+use assert_cmd::Command;
+
+[#test]
+fn runs(){
+    //在这里的test case 是如何和主程序关联上的，不清楚，我在修改了cargo_bin参数之后，cargo test 还是
+    //运行的main.rs 有点不能理解
+    let mut cmd = Command::cargo_bin("hello").unwarp();
+    cmd.assert().success();
+}
+
+fn true_ok(){
+    let mut cmd = Command::cargo_bin("true").unwarp();
+    cmd.assert().success();
+}
