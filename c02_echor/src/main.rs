7975df70361e763afda269b26098a673ce1b23ec
Karl Cen (EXT-NSB) <karl.cen.ext@nokia-sbell.com>
2023-12-19 15:08:41 +0800

    befor continue working on C02

--- c02_echor/src/main.rs
+++ c02_echor/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App,Arg};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Karl Cen <karl.cenliming@gmail.com>")
+        .about("Rust version echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("do not print newline")
+            .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:#?}", matches);
 }
